6f711a6b276752dd3845a6b6a9790a079e4d8fe6
Adelar Tiemann Junior <adelar.junior@asigra.com>
2025-10-29 12:27:47 -0300

    refactor: Update UserCollectionItem entity and routes - executed using vibe code

--- src/App/Entity/UserCollectionItem.php
+++ src/App/Entity/UserCollectionItem.php
@@ -5,31 +5,50 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Repository\UserCollectionItemRepository;
-use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserCollectionItemRepository::class)]
+#[ORM\Table(name: 'user_collection_items')]
 class UserCollectionItem
 {
-    public function __construct(
-        #[ORM\ManyToOne(targetEntity: User::class)]
-        #[ORM\JoinColumn(nullable: false)]
-        private User $user,
-        #[ORM\ManyToOne(targetEntity: MtgoItem::class)]
-        #[ORM\JoinColumn(nullable: false)]
-        private MtgoItem $mtgoItem,
-        #[ORM\Column(type: Types::INTEGER)]
-        private int $quantity = 1,
-        #[ORM\Id]
-        #[ORM\GeneratedValue]
-        #[ORM\Column(type: Types::INTEGER)]
-        private ?int $id = null,
-    ) {
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'id', nullable: false)]
+    private User $user;
+
+    #[ORM\ManyToOne(targetEntity: MtgoItem::class)]
+    #[ORM\JoinColumn(name: 'mtgo_item_id', referencedColumnName: 'id', nullable: false)]
+    private MtgoItem $mtgoItem;
+
+    #[ORM\Column(type: 'integer')]
+    private int $quantity = 0;
+
+    public function __construct(User $user, MtgoItem $mtgoItem, int $quantity = 1)
+    {
+        $this->user     = $user;
+        $this->mtgoItem = $mtgoItem;
+        $this->quantity = $quantity;
     }
 
+    // Getters and Setters
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+        return $this;
     }
 
     public function getMtgoItem(): MtgoItem
@@ -51,17 +70,6 @@ class UserCollectionItem
     public function setQuantity(int $quantity): self
     {
         $this->quantity = $quantity;
-        return $this;
-    }
-
-    public function getUser(): User
-    {
-        return $this->user;
-    }
-
-    public function setUser(User $user): self
-    {
-        $this->user = $user;
         return $this;
     }
 }
